7180efc775ce8bc1ac021fbb2d3a7b375b9c9aa6
Matt Price <matt@mattprice.eu>
2024-11-12 22:15:56 -0800

    Added CSV output for accounts

--- vaultReport.ps1
+++ vaultReport.ps1
@@ -27,12 +27,13 @@ param (
 #$PVWAUrl = "https://pvwa.basgiath.uk/PasswordVault/"
 $PVWAAuthSuffix = "API/auth/Cyberark/Logon/"
 $PVWALogonUrl= $PVWAUrl + $PVWAAuthSuffix
-$PVWAGetAccountsSuffix = "API/Accounts"
+$PVWAGetAccountsSuffix = 'API/Accounts'
 $PVWAAccountsUrl = $PVWAUrl + $PVWAGetAccountsSuffix
 $PVWAAccountActivitySuffix = "WebServices/PIMServices.svc/Accounts/"
 $PVWAGetUsersSuffix = "API/Users?ExtendedDetails=true"
 $PVWAGetUsersUrl = $PVWAUrl + $PVWAGetUsersSuffix
 #$PVWACreds = Get-Credential
+$accountsProcessed = 0
 #endregion
 
 
@@ -53,11 +54,15 @@ function Request-PvwaAuthToken {
 }
 
 function Get-Accounts {
-    (Invoke-WebRequest -Uri $PVWAAccountsUrl -Method Get -Headers @{'Authorization' = "$AuthTrimmed"} -ContentType "application/json").Content
+    param(
+        $offset
+    )
+    $PaginatedPVWAAccountsURL = $PVWAAccountsUrl + '?offset=' + $offset + '&limit=50'
+    (Invoke-WebRequest -Uri $PaginatedPVWAAccountsURL -Method Get -Headers @{'Authorization' = "$AuthTrimmed"} -ContentType "application/json").Content
 }
 
 # name,address,userName,platformId,safeName,createdTime
-
+<#
 function Get-AccountDetails {
     param (
         [psobject]$Account
@@ -67,7 +72,7 @@ function Get-AccountDetails {
     Invoke-WebRequest -Uri $uri -Method Get -Headers @{'Authorization' = "$AuthTrimmed"} -ContentType "application/json"
     Add-Member -InputObject $Account -NotePropertyName "TestName" -NotePropertyValue "TestValue2"
 }
-
+#>
 function Get-Users{
     (Invoke-WebRequest -Uri $PVWAGetUsersUrl -Method Get -Headers @{'Authorization' = "$AuthTrimmed"} -ContentType "application/json").Content
 }
@@ -78,12 +83,30 @@ function Get-Users{
 
 #region Run reports
 $AuthTrimmed = Request-PvwaAuthToken -cred $PVWACreds
-[psobject[]]$Accounts = (Get-Accounts | ConvertFrom-Json).value
-foreach($Account in $Accounts){
-    (Get-AccountDetails -Account $Account).Content | ConvertFrom-Json
+$MoreAccountsToProcess = $true
+$offset = 0
+$offsetIncrement = 50
+while ($MoreAccountsToProcess){
+    [psobject[]]$Accounts += (Get-Accounts -offset $offset | ConvertFrom-Json).value
+    if ($offset -le $Accounts.Count){
+    $offset = ($offset + $offsetIncrement)
+    #Write-Host $offset "accounts processed so far"
+    Write-Progress -Activity "Exporting accounts" -Status "$offset accounts processed so far" -PercentComplete -1
+    }
+    else {
+        $MoreAccountsToProcess = $false
+        Write-Host $offset "accounts processed in total"
+    }
 }
+#foreach($Account in $Accounts){
+#    (Get-AccountDetails -Account $Account).Content | ConvertFrom-Json
+#}
 $Accounts[0]
+
 [psobject[]]$Users = (Get-Users | ConvertFrom-Json).Users
-$Users
 $Users.count
+$Users[0] | Select-Object -Property name
+$Accounts | Select-Object -Property name,address,userName,id,platformId,safeName,createdTime,secretManagement,platformAccountProperties | Export-Csv -Path .\output.csv
+
+
 #endregion
